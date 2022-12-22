66dedfc42d8472c00a027b3df2a9b78e68f3ff24
JamieCollins <Teyouneek@hotmail.com>
2022-12-22 07:34:29 +0000

    Add function to create game board

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def create_game_board():
+    for i in range(6):
+            for j in range(7):
+                print(". ", end = "")
+
+            print()
+
+
+create_game_board()
