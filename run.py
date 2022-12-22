741c6fea40654369a389ad810de7762d86945209
JamieCollins <Teyouneek@hotmail.com>
2022-12-22 08:40:24 +0000

    Add code to drop token into game upon user input

--- run.py
+++ run.py
@@ -1,19 +1,37 @@
+rows = 6
+cols = 7
+
+
+#def create_game_board():
+#   """
+#   Creates a table of 6 rows and 7 columns to be used as the game board.
+#   """
+#  for i in range(rows):
+#          for j in range(cols):
+#               print(". ", end="")
+#
+#           print()
+
+
+game_grid = [[". ", ". ", ". ", ". ", ". ", ". ", ". "],
+            [". ", ". ", ". ", ". ", ". ", ". ", ". "],
+            [". ", ". ", ". ", ". ", ". ", ". ", ". "],
+            [". ", ". ", ". ", ". ", ". ", ". ", ". "],
+            [". ", ". ", ". ", ". ", ". ", ". ", ". "],
+            [". ", ". ", ". ", ". ", ". ", ". ", ". "],]
+
 def create_game_board():
     """
     Creates a table of 6 rows and 7 columns to be used as the game board.
     """
-    for i in range(6):
-            for j in range(7):
-                print(". ", end="")
-
-            print()
-
-
-create_game_board()
+    for i in game_grid:
+        print(*i)
 
 
 def player_guess():
-    player_column_select = int(input("Please enter a number between 0 and 6: "))
-    print(player_column_select)
+    col = int(input("Please enter a number between 0 and 6: "))
+    game_grid[5][col] = "O"
+    create_game_board()
+
 
 player_guess()
