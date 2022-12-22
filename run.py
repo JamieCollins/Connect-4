5af114368c2b888ad2cf3cde00deb73ea6bb72cb
JamieCollins <Teyouneek@hotmail.com>
2022-12-22 09:06:40 +0000

    Add function to verify token placement

--- run.py
+++ run.py
@@ -1,7 +1,3 @@
-rows = 6
-cols = 7
-
-
 #def create_game_board():
 #   """
 #   Creates a table of 6 rows and 7 columns to be used as the game board.
@@ -20,6 +16,7 @@ game_grid = [[". ", ". ", ". ", ". ", ". ", ". ", ". "],
             [". ", ". ", ". ", ". ", ". ", ". ", ". "],
             [". ", ". ", ". ", ". ", ". ", ". ", ". "],]
 
+
 def create_game_board():
     """
     Creates a table of 6 rows and 7 columns to be used as the game board.
@@ -29,9 +26,32 @@ def create_game_board():
 
 
 def player_guess():
+    """
+    Asks user to choose a column on the grid in which to drop a token.
+    """
+    global col
     col = int(input("Please enter a number between 0 and 6: "))
-    game_grid[5][col] = "O"
-    create_game_board()
+    verify_token_placement()
 
 
-player_guess()
+def verify_token_placement():
+    """
+    Verifies the placement of the tokens on the board.
+    """
+    rows = 5
+
+    if game_grid[rows][col] == "O":
+        rows = rows - 1
+        game_grid[rows][col] = "O"
+        create_game_board()
+        player_guess()
+    else:
+        game_grid[rows][col] = "O"
+        create_game_board()
+        player_guess()
+
+
+def game_start():
+    player_guess()
+
+game_start()
