83cde9e0e0e8e1b76def3d74921e06bd85ccdfa9
JamieCollins <Teyouneek@hotmail.com>
2022-12-22 07:47:03 +0000

    Add function that prompts user to select row on board for token to be dropped

--- run.py
+++ run.py
@@ -1,9 +1,19 @@
 def create_game_board():
+    """
+    Creates a table of 6 rows and 7 columns to be used as the game board.
+    """
     for i in range(6):
             for j in range(7):
-                print(". ", end = "")
+                print(". ", end="")
 
             print()
 
 
 create_game_board()
+
+
+def player_guess():
+    player_column_select = int(input("Please enter a number between 0 and 6: "))
+    print(player_column_select)
+
+player_guess()
