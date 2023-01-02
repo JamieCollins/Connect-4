f0b6644e9fddb3e09ffe09fbb5eb6afad547fc3d
JamieCollins <Teyouneek@hotmail.com>
2023-01-02 00:38:56 +0000

    Added function to stop game loop if player wins

--- run.py
+++ run.py
@@ -37,34 +37,34 @@ def verify_token_placement():
     if game_grid[rows][col] == ".":
         game_grid[rows][col] = "O"
         create_game_board()
-        game_start()
+        end_game()
     elif game_grid[rows - 1][col] == ".":
         game_grid[rows - 1][col] = "O"
         create_game_board()
-        game_start()
+        end_game()
     elif game_grid[rows - 2][col] == ".":
         game_grid[rows - 2][col] = "O"
         create_game_board()
-        game_start()
+        end_game()
     elif game_grid[rows - 3][col] == ".":
         game_grid[rows - 3][col] = "O"
         create_game_board()
-        game_start()
+        end_game()
     elif game_grid[rows - 4][col] == ".":
         game_grid[rows - 4][col] = "O"
         create_game_board()
-        game_start()
+        end_game()
     elif game_grid[rows - 5][col] == ".":
         game_grid[rows - 5][col] = "O"
         create_game_board()
-        game_start()
+        end_game()
     else:
         print("ERROR")
     
 
 def player_2_guess():
     """
-    Verifies the placement of the tokens on the board for the cpu,
+    Verifies the placement of the tokens on the board for the ai,
     randint is used to choose a random row.
     """
     print("\n")
@@ -73,34 +73,34 @@ def player_2_guess():
     if game_grid[rows][col] == ".":
         game_grid[rows][col] = "X"
         create_game_board()
-        game_start()
+        end_game()
     elif game_grid[rows - 1][col] == ".":
         game_grid[rows - 1][col] = "X"
         create_game_board()
-        game_start()
+        end_game()
     elif game_grid[rows - 2][col] == ".":
         game_grid[rows - 2][col] = "X"
         create_game_board()
-        game_start()
+        end_game()
     elif game_grid[rows - 3][col] == ".":
         game_grid[rows - 3][col] = "X"
         create_game_board()
-        game_start()
+        end_game()
     elif game_grid[rows - 4][col] == ".":
         game_grid[rows - 4][col] = "X"
         create_game_board()
-        game_start()
+        end_game()
     elif game_grid[rows - 5][col] == ".":
         game_grid[rows - 5][col] = "X"
         create_game_board()
-        game_start()
+        end_game()
     else:
         print("ERROR")
 
 
 def game_fin():
     """
-    Checks to see if either player has ammassed 4 tokens either
+    Checks to see if user player has ammassed 4 tokens either
     horizontally, vertically or diagonally.
     """
     for col in range(7):
@@ -110,17 +110,56 @@ def game_fin():
                     and game_grid[rows - 2][col] == "O"
                         and game_grid[rows - 3][col] == "O"):
                             print("Congratulations, you won vertically!")
+                            return True
             elif (game_grid[rows][col] == "O"
                 and game_grid[rows][col - 1] == "O"
                     and game_grid[rows][col - 2] == "O"
                         and game_grid[rows][col - 3] == "O"):
                             print("Congratulations, you won horizontally!")
+                            return True
             elif (game_grid[rows][col] == "O"
                 and game_grid[rows - 1][col - 1] == "O"
                     and game_grid[rows - 2][col - 2] == "O"
                         and game_grid[rows - 3][col - 3] == "O"):
                             print("Congratulations, you won diagonally!")
+                            return True
 
+
+def game_fin_ai():
+    """
+    Checks to see if the AI opponent has ammassed 4 tokens either
+    horizontally, vertically or diagonally.
+    """
+    for col in range(7):
+        for rows in range(6):
+            if (game_grid[rows][col] == "X"
+                and game_grid[rows - 1][col] == "X"
+                    and game_grid[rows - 2][col] == "X"
+                        and game_grid[rows - 3][col] == "X"):
+                            print("Unfortunately you lost!")
+                            return True
+            elif (game_grid[rows][col] == "X"
+                and game_grid[rows][col - 1] == "X"
+                    and game_grid[rows][col - 2] == "X"
+                        and game_grid[rows][col - 3] == "X"):
+                            print("Unfortunately you lost!")
+                            return True
+            elif (game_grid[rows][col] == "O"
+                and game_grid[rows - 1][col - 1] == "X"
+                    and game_grid[rows - 2][col - 2] == "X"
+                        and game_grid[rows - 3][col - 3] == "X"):
+                            print("Unfortunately you lost!")
+                            return True
+
+
+def end_game():
+    """
+    Ends game if either users matches 4 tokens.
+    """
+    if game_fin() or game_fin_ai() == True:
+        print("Game Over")
+    else:
+        game_start()
 
 
 def game_start():
@@ -128,7 +167,6 @@ def game_start():
     Initialises game turn. If number is odd, player chooses a row to drop token,
     if even it is the cpu's turn.
     """
-    game_fin()
     global turn
     turn = turn + 1
     if turn % 2 == 0:
